1677ceeb388375942392ecbcaedd6f4e49706244
iRyna <irynm61@gmail.com>
2022-01-24 15:26:23 -0600

    2st commit

--- src/iRyn.java
+++
@@ -1,6 +0,0 @@
-public class iRyn {
-    public static void main(String[] args) {
-        System.out.println("b25");
-    }
-    // Hi guys
-}
---
+++ src/iryn/iryn.java
@@ -0,0 +1,4 @@
+package iryn;
+
+public class iryn {
+}
